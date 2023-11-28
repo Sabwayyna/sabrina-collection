771be4f61028d8a1b3f7a3401b970ec38684b182
Sabwaynaa <wus020@newschool.edu>
2023-11-28 11:04:06 -0500

    entry-11 revision

--- entry-11/script.js
+++ entry-11/script.js
@@ -3,9 +3,9 @@ $( document ).ready(function() {
 	"use strict";
 	
     $('#fullpage').fullpage({
-		//responsiveWidth: 812,
+		
 		responsiveWidth: 1141,
 		responsiveSlides: true,
 	});
 	
-}); /* END: document.ready*/
+}); 
