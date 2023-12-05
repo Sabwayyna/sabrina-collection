90463ecb6b5690d021b58ee09f915dd08abc8900
Sabwaynaa <wus020@newschool.edu>
2023-12-05 01:34:28 -0500

    revise

--- entry-11/script.js
+++ entry-11/script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function () {
 	setTimeout(function () {
 	  document.getElementById("fade-in-overlay").style.display = "none";
-	}, 3000); // 6000 milliseconds = 6 seconds
+	}, 3000);
   });
   
